8389540520e8794fa9065801c188405d3e6ca552
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2023-04-23 10:24:45 -0400

    Update Button.jsx
    Cc

--- components/Button.jsx
+++ components/Button.jsx
@@ -4,7 +4,7 @@ export default function Button (props) {
     return (
              <button 
                 className={`${style.button} ${style[props.style]}`}
-                onClick={props.click}>
+                onClick={props.click}> 
                 {props.children}
             </button>
     )  
